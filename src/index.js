6c73bafd28388fb822b9aee44ac32ed83a9ef295
tacticalDoor3000 <obivan085@gmail.com>
2024-09-09 11:53:19 +0300

    no message

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-import template from './footer.twig';
+import  './footer.twig'
 import './stylesheet.scss'
+import './index.twig'
 
 
-function component() {
-  const element = document.createElement('footer');
-
-  const html = template();
-  console.log(html);
-  //document.getElementById('app').innerHTML = html;
-  element.innerHTML = html;
-  //element.classList.add('hello');
-
-  return element;
-
-}
 
 
-// => returns pre-compiled template as a function and automatically includes Twig.js to your project
- 
-
-
-document.body.appendChild(component());
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,24 +4,31 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 const loader = require('sass-loader');
+const { prototype } = require('events');
 
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: './index.js',
   mode: 'development',
-  devtool: 'inline-source-map',
+  //devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist'),
     filename: 'bundle.js',
   },
   //watch: true,
+  devServer: {
+    static: './dist',
+    port: 9000,
+  },
   module: {
+    
     rules: [
       {
         test: /\.txt$/, 
         use: "raw-loader",
       },
+      /*
       {
         test: /\.s[ac]ss$/i, 
         use: [
@@ -30,21 +37,30 @@ module.exports = {
           "sass-loader",
         ]
       },
+      */
       {
         test: /\.twig$/,
         use: {
           loader: 'twig-loader',
           options: {
               // See options section below
+              
           },
         }
       },
+      
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      
       {
         test: /\.(sa|sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, 
           'css-loader', 
           'sass-loader'],
       },
+      
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
@@ -58,13 +74,13 @@ module.exports = {
       },
     ]
   },
+  
   plugins: [
-    
-    
-    new HtmlWebpackPlugin({ filename: '_index.html'}),
+    new HtmlWebpackPlugin( {filename: '.dist/apl.index.html'}),
+    new HtmlWebpackPlugin({ 
+      template: './index.twig',
+      inject: 'body'}),
     new MiniCssExtractPlugin({ filename: 'stylesheet.css'}),
-   
-      
   ],
   
 }
