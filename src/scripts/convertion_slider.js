868ee286530b0052765bcec7cf7dc42e24a9e41a
tacticalDoor3000 <obivan085@gmail.com>
2024-12-06 18:45:50 +0300

    no message

--- src/scripts/convertion_slider.js
+++ src/scripts/convertion_slider.js
@@ -5,15 +5,11 @@ var btnNext = document.querySelector('.convert__btn_next');
 var pagCounter = document.querySelectorAll('.convert__pag-dot');
 
 var sliderCurrentElIndex = 0;
-var pagDots = 0;
-var sliderLength = 7;
-//check array slidersPack
 
 
-//slideChanger(pagDots);
 
 var slidesChunk = document.querySelectorAll('.convertion__item');
-var slidesCathed = [];
+var slidesCatche = [];
 var slidersPack = [];
 
 
@@ -24,95 +20,89 @@ btnPrev.addEventListener('click', () => {
   console.log ('btn');
   if (sliderCurrentElIndex > 0) {
     sliderCurrentElIndex = sliderCurrentElIndex - 1;
-    
+    slideChanger(sliderCurrentElIndex);
+    paginationDotsChanger(sliderCurrentElIndex);
   }
-  if (pagDots > 0) {
-    pagDots = pagDots - 1;
-  }
-  slideChanger(sliderCurrentElIndex, 'prev');
-  paginationDotsChanger(pagDots);
   
 });
 
 btnNext.addEventListener('click', () => {
   
-  console.log ('btn');
   if ( sliderCurrentElIndex < slidersPack.length) {
-    sliderCurrentElIndex = ++sliderCurrentElIndex % sliderLength;
-    pagDots = ++pagDots % (slidersPack.length - 2);
+    sliderCurrentElIndex = ++sliderCurrentElIndex % slidersPack.length;
     
+    slideChanger(sliderCurrentElIndex);
+    paginationDotsChanger(sliderCurrentElIndex);
   }
-  slideChanger(sliderCurrentElIndex, 'next');
-  paginationDotsChanger(pagDots);
   
 });
 function sliderConstructor () {
 
-  slidesCathed.push(slidesChunk[0]);
-  slidesCathed.push(slidesChunk[1]);
+  /*
+  let attrChunk = [];
+  for(let i =0; i < slidesChunk.length; i++ ) {
+    if(slidesChunk[i].getAttribute('data-slid') != 'none') {
+      attrChunk.push(slidesChunk[i]);
+      
+    }
+    slidersPack.push(slidesChunk[i]);
+  }
+  console.log(attrChunk);
+  */
 
-  slidersPack.push(slidesCathed);
+  
+  slidesCatche = [slidesChunk[0],slidesChunk[1]];
+
+  slidersPack.push(slidesCatche);
 
   slidersPack.push(slidesChunk[2]);
 
-  slidesCathed = [slidesChunk[3],slidesChunk[4]];
+  slidesCatche = [slidesChunk[3],slidesChunk[4]];
 
-  slidersPack.push(slidesCathed);
+  slidersPack.push(slidesCatche);
 
   slidersPack.push(slidesChunk[5]);
   slidersPack.push(slidesChunk[6]);
-  slidersPack.push(slidesChunk[7]);
-  slidersPack.forEach((el) => {
-    console.log(el);
-  });
+  
+  
+  
 }
-function slideChanger(index, direction) {
-  console.log('slide changer');
 
-  if (index == 1 && direction == 'prev') {
-    slideHide();
-    slideDoble(index, direction);
-  }
-  else if (index == 4 && direction == "prev") {
-    slideHide();
-    slideDoble(index, direction); 
-  }
-  else if (index == 0) {
-    slideHide();
-    slideDoble(index, direction);
-  }
-  else if (index == 3) {
-    slideHide();
-    slideDoble(index, direction);
-    
-  } else {
-    slideHide();
+function slideChanger(index) {
+  
+  slideShow(index);
+}
+function slideShow(index) {
+  slideHide();
+  
+  if (!Array.isArray(slidersPack[index])) {
     slidersPack[index].classList.add('convertion__item_active');
+  } else {
+    slidersPack[index].forEach((el) => {
+      el.classList.add('convertion__item_active');
+    })
   }
   
 }
+function slideHide() {
+  slidersPack.forEach((el) => {
+    if (Array.isArray(el)) {
+      el.forEach((sub_el) => {
+        sub_el.classList.remove('convertion__item_active');
+      })
+    }else {
+      el.classList.remove('convertion__item_active');
+    }
+    
+  })
+}
+
 function sliderInit() {
   slidersPack[0].forEach((el) => {
     el.classList.add('convertion__item_active');
   })
 }
-function slideDoble(index, direction) {
-  console.log ('slidedoble');
-  if (direction == 'next') {
-    console.log ('forward');
-    slidersPack[index].classList.add('convertion__item_active');
-    slidersPack[index + 1].classList.add('convertion__item_active');
-    sliderCurrentElIndex++;
-    console.log(sliderCurrentElIndex);
-  } else if (direction == 'prev') {
-    console.log ('backward');
-    slidersPack[index].classList.add('convertion__item_active');
-    slidersPack[index - 1].classList.add('convertion__item_active');
-    sliderCurrentElIndex--;
-    console.log(sliderCurrentElIndex);
-  }
-  
-}
+
 function paginationDotsChanger(index) {
   pagCounter.forEach((el, index) => {
     el.classList.remove('convert__pag-dot--active');
@@ -123,9 +113,4 @@ function paginationDotsChanger(index) {
    catch(err){
     console.log(err);
    }
-}
-function slideHide() {
-  slidersPack.forEach((el) => {
-    el.classList.remove('convertion__item_active');
-  })
 }
